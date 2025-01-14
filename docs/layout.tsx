6082246d099fcc6ff9f46e7ea7d823065ed61fe5
Syntax <2079305+TheUltDev@users.noreply.github.com>
2025-01-13 21:02:05 -0600

    fix: vocs error

--- docs/layout.tsx
+++ docs/layout.tsx
@@ -1,3 +1,14 @@
+// Temp: workaround for vocs
+if (typeof navigator === 'undefined') {
+  console.log('navigator is undefined');
+  globalThis.navigator = {
+    clipboard: {
+      // @ts-ignore
+      writeText: () => {},
+    }
+  };
+}
+
 export default function Layout(props: React.PropsWithChildren) {
   return props.children;
 }
