66b1568036fba4c229d784974fb5d671764f3255
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-09-20 16:09:32 -0500

    fix text input type

--- src/widgets/textinput/TextInput.tsx
+++ src/widgets/textinput/TextInput.tsx
@@ -1,8 +1,6 @@
 import {TextInput as RNTextInput} from 'react-native';
 import {withUnistyles} from 'react-native-unistyles';
 
-import type {TextInputComponent} from 'react-native';
-
 export const TextInput = withUnistyles(RNTextInput, (theme: any) => ({
   placeholderTextColor: theme?.colors?.mutedForeground,
-})) as unknown as TextInputComponent;
+})) as unknown as typeof RNTextInput;
