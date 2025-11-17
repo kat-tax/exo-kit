0f8fa2b2bb13398a9c4d3d9449fb74fc47f5026a
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-11-17 14:04:46 -0600

    fix fs in web worker, add bytesize utility

--- src/services/fs/Fs.export.ts
+++ src/services/fs/Fs.export.ts
@@ -17,3 +17,17 @@ export const cancelHash = FS.cancelHash;
 export const isTextFile = FS.isTextFile;
 export const importFiles = FS.importFiles;
 export const getDiskSpace = FS.getDiskSpace;
+
+/**
+ * Converts a number of bytes to a human-readable string.
+ * @param bytes The number of bytes to convert.
+ * @param scale The scale to use (1000 or 1024) default is 1000.
+ * @returns A human-readable string.
+ */
+export function bytesize(bytes: number, scale: 1000 | 1024 = 1000): string {
+  const a = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  let b = bytes;
+  let u = 0;
+  while (b >= scale || -b >= scale) {b /= scale; u++}
+  return `${u ? b.toFixed(1) : b} ${a[u]}`;
+}
